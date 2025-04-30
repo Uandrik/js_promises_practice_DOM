f3eca0a356c42f981a61ca7158c56073e1917167
Uandrik <uandriy4141@gmail.com>
2025-04-30 06:35:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,49 +3,55 @@
 const body = document.querySelector('body');
 
 const promiseOne = new Promise((resolve, reject) => {
-  document.addEventListener(
-    'click',
-    () => {
-      clearTimeout(rejectionTimer);
-      resolve(`First promise was resolved`);
-    },
-    { once: true },
-  );
+  const mouseDown = (e) => {
+    clearTimeout(rejectionTime);
+    resolve(`First promise was resolved`);
+    document.removeEventListener('mousedown', mouseDown);
+  };
 
-  const rejectionTimer = setTimeout(() => {
+  document.addEventListener('mousedown', mouseDown);
+
+  const rejectionTime = setTimeout(() => {
     reject(new Error(`First promise was rejected`));
   }, 3000);
 });
 
 const promiseTwo = new Promise((resolve, reject) => {
-  document.addEventListener(
-    'mousedown',
-    (e) => {
-      if (e.button === 0 || e.button === 2) {
-        resolve(`Second promise was resolved`);
-      }
-    },
-    { once: true },
-  );
+  let isResolved = false;
+
+  const mouseDown = (e) => {
+    if ((e.button === 0 || e.button === 2) && !isResolved) {
+      resolve(`Second promise was resolved`);
+      isResolved = true;
+      document.removeEventListener('mousedown', mouseDown);
+    }
+  };
+
+  document.addEventListener('mousedown', mouseDown);
 });
 
 const promiseThree = new Promise((resolve, reject) => {
-  let leftButton = false;
-  let rightButton = false;
+  let leftMouse = false;
+  let rightMouse = false;
+  let isResolved = false;
 
-  document.addEventListener('mousedown', (e) => {
+  const mouseDown = (e) => {
     if (e.button === 0) {
-      leftButton = true;
+      leftMouse = true;
     }
 
     if (e.button === 2) {
-      rightButton = true;
+      rightMouse = true;
     }
 
-    if (leftButton && rightButton) {
+    if (leftMouse && rightMouse && !isResolved) {
+      isResolved = true;
       resolve(`Third promise was resolved`);
+      document.removeEventListener('mousedown', mouseDown);
     }
-  });
+  };
+
+  document.addEventListener('mousedown', mouseDown);
 });
 
 function notificationHandler(type, message) {
